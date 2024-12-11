e889816b78988de9a080dd8852165ec1f28cba1f
Tushar <tushar.mangla1120@gmail.com>
2024-12-11 22:33:47 +0530

    added context and toast

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ToastContainer } from 'react-toastify';
+import { AppProvider } from './services/context/AppContext.jsx'
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <ToastContainer />
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
