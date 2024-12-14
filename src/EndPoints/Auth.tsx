fe0db7d6042e9eddf8d8e9d3b725ad866d28167d
Tushar <tushar.mangla1120@gmail.com>
2024-12-14 18:20:38 +0530

    added verify otp endpoints

--- src/EndPoints/Auth.tsx
+++ src/EndPoints/Auth.tsx
@@ -1,5 +1,6 @@
 export const AUTHENDPOINTS = {
     login:"/auth/login",
     signup:"/auth/signup",
-    sentotp:"/auth/send-otp"
+    sentotp:"/auth/send-otp",
+    verifyotp:"/auth/verify-otp"
 }
