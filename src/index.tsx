e764a3c79bada3b4c4c7bfcc83d4bb6770563c82
Tushar <tushar.mangla1120@gmail.com>
2024-12-14 18:20:15 +0530

    added toast style

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ToastContainer } from 'react-toastify';
-import { AppProvider } from './services/context/AppContext.jsx'
+import { AppProvider } from "./services/context/AppContext";
+import 'react-toastify/dist/ReactToastify.css';
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
