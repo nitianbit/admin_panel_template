134a80a323904f3ab1f0a187b6d35e57fa964969
Tushar <tushar.mangla1120@gmail.com>
2024-12-22 19:34:10 +0530

    added patient table

--- src/components/Dialog/PatientDialog.tsx
+++ src/components/Dialog/PatientDialog.tsx
@@ -1,10 +1,9 @@
-import { Button, Chip, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, Paper, Slide, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, FormControlLabel, Checkbox } from "@mui/material"
+import { Button, Dialog, DialogActions, DialogTitle,  Slide  } from "@mui/material"
 import React, { useEffect, useState } from "react"
-import { doGET } from "../../utils/HttpUtils"
-import { DOCTORENDPOINTS } from "../../EndPoints/Doctor"
 import { useAppContext } from "../../services/context/AppContext"
-import { isError } from "../../utils/helper"
 import { TransitionProps } from "@mui/material/transitions"
+import { usePatientStore } from "../../services/patient"
+import PatientTable from "../../pages/PatientInfo/PatientTable"
 
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & {
@@ -23,32 +22,16 @@ interface PatientDialogProps {
 }
 
 const PatiendDialog: React.FC<PatientDialogProps> = ({ patients = [], open, handleClose, handleSave }) => {
-    const { success, error, userData } = useAppContext()
-    const [filters, setFilters] = useState({
-        doctor: userData?._id
-    });
 
 
-    const [selectedPatients, setSelectedPatients] = useState<string[]>([]);
+    const { data, totalPages, rows, currentPage, filters, isLoading, onPageChange, fetchGrid, create, setFilters, nextPage, prevPage } = usePatientStore();
 
-    const handleCheckboxChange = (patientId: string) => {
-        setSelectedPatients(prevSelected => {
-            if (prevSelected.includes(patientId)) {
-                return prevSelected.filter(id => id !== patientId);
-            } else {
-                return [...prevSelected, patientId];
-            }
-        });
-    };
 
-    const handleSelectAllChange = () => {
-        if (selectedPatients.length === patients.length) {
-            setSelectedPatients([]); // Deselect all if all are selected
-        } else {
-            setSelectedPatients(patients?.map(patient => patient.id)); // Select all patients
+    useEffect(()=>{
+        if(data.length == 0){
+            fetchGrid()
         }
-    };
-
+    },[])
 
 
     return (
@@ -56,78 +39,13 @@ const PatiendDialog: React.FC<PatientDialogProps> = ({ patients = [], open, hand
             open={open}
             onClose={handleClose}
             TransitionComponent={Transition}
-            maxWidth="xs"
+            maxWidth="lg"
             fullWidth
             sx={{ height: "100%" }}
         >
             <DialogTitle>Patients</DialogTitle>
 
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="patient table">
-                    <TableHead>
-                        <TableRow>
-                            {patients?.length > 0 && <TableCell align="center">
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            checked={selectedPatients.length === patients.length}
-                                            onChange={handleSelectAllChange}
-                                        />
-                                    }
-                                    label="Select All"
-                                />
-                            </TableCell>}
-                            <TableCell align="center">#</TableCell>
-                            <TableCell>FULL NAME</TableCell>
-                            <TableCell>GENDER</TableCell>
-                            <TableCell>Phone no</TableCell>
-                            <TableCell>AGE</TableCell>
-                            <TableCell>REFERRED BY DR.</TableCell>
-                            <TableCell>APPOINTMENT DATE</TableCell>
-                            <TableCell>ASSIGNED DR.</TableCell>
-                            <TableCell>STATUS</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {patients.length > 0 &&
-                            patients?.map((appointment: any, index: any) => (
-                                <TableRow
-                                    key={index}
-                                    // component={Link}
-                                    // to={`/patient-detail/${appointment.id}`}
-                                    style={{ textDecoration: "none", color: "inherit" }}
-                                >
-                                    <TableCell align="center">
-                                        <FormControlLabel
-                                            control={
-                                                <Checkbox
-                                                    checked={selectedPatients.includes(appointment.id)}
-                                                    onChange={() => handleCheckboxChange(appointment.id)}
-                                                />
-                                            }
-                                            label=""
-                                        /></TableCell>
-                                    <TableCell align="center">{appointment.id}</TableCell>
-                                    <TableCell>{appointment.fullName}</TableCell>
-                                    <TableCell>{appointment.gender}</TableCell>
-                                    <TableCell>{appointment.phone}</TableCell>
-                                    <TableCell>{appointment.age}</TableCell>
-                                    <TableCell>{appointment.referredByDoctor}</TableCell>
-                                    <TableCell>{appointment.appointmentDate}</TableCell>
-                                    <TableCell>{appointment.assignedDoctor}</TableCell>
-                                    <TableCell>
-                                        <Chip
-                                            label={appointment.status}
-                                            variant="outlined"
-                                            color={appointment.status === "open" ? "success" : "error"}
-                                            sx={{ textTransform: "uppercase" }}
-                                        />
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+           <PatientTable/>
             <DialogActions>
                 <Button onClick={handleClose}>Cancel</Button>
                 <Button onClick={handleSave} type="submit" variant="contained">
