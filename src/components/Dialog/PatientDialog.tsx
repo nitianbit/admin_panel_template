f440a5649530b94d8e5eff5f0becaa8ef465736f
Tushar <tushar.mangla1120@gmail.com>
2024-12-22 21:00:26 +0530

    selected ids for patient

--- src/components/Dialog/PatientDialog.tsx
+++ src/components/Dialog/PatientDialog.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogActions, DialogTitle,  Slide  } from "@mui/material"
+import { Button, Dialog, DialogActions, DialogTitle, Slide } from "@mui/material"
 import React, { useEffect, useState } from "react"
 import { useAppContext } from "../../services/context/AppContext"
 import { TransitionProps } from "@mui/material/transitions"
@@ -18,20 +18,21 @@ interface PatientDialogProps {
     open: boolean;
     patients: any[];
     handleClose: () => void;
-    handleSave: () => void;
+    handleSave: (e:any) => void;
 }
 
 const PatiendDialog: React.FC<PatientDialogProps> = ({ patients = [], open, handleClose, handleSave }) => {
 
+    const [selectedIds, setSelectedIds] = useState([]);
 
     const { data, totalPages, rows, currentPage, filters, isLoading, onPageChange, fetchGrid, create, setFilters, nextPage, prevPage } = usePatientStore();
 
 
-    useEffect(()=>{
-        if(data.length == 0){
+    useEffect(() => {
+        if (data.length == 0) {
             fetchGrid()
         }
-    },[])
+    }, [])
 
 
     return (
@@ -45,10 +46,15 @@ const PatiendDialog: React.FC<PatientDialogProps> = ({ patients = [], open, hand
         >
             <DialogTitle>Patients</DialogTitle>
 
-           <PatientTable/>
+            <PatientTable
+                selectedIds={selectedIds}
+                setSelectedIds={setSelectedIds}
+            />
             <DialogActions>
                 <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleSave} type="submit" variant="contained">
+                <Button onClick={()=>{
+                    handleSave(selectedIds)
+                }} type="submit" variant="contained">
                     Submit
                 </Button>
             </DialogActions>
