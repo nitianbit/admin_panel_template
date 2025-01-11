003cb719087835cda94c76bed54b537b403ce644
Tushar <tushar.mangla1120@gmail.com>
2025-01-12 00:00:06 +0530

    company changes

--- src/pages/Company/CompanyDialog.tsx
+++ src/pages/Company/CompanyDialog.tsx
@@ -33,6 +33,10 @@ export default function CompanyDialog({
 
   const [comapnyData, setCompanyData] = React.useState<Company>({
     name: "",
+    email: "",
+    phone: "",
+    contactPerson: "",
+    noOfEmployees: 0,
     website: "",
     isActive: true
   })
@@ -85,22 +89,60 @@ export default function CompanyDialog({
         sx={{ height: "100%" }}
       >
 
-<form onSubmit={handleSave}>
-        <DialogTitle>Company Details</DialogTitle>
+        <form onSubmit={handleSave}>
+          <DialogTitle>Company Details</DialogTitle>
 
-        <DialogContent dividers>
-          <TextField
-            margin="dense"
-            id="name"
-            label="Company Name"
-            type="name"
-            required
-            fullWidth
-            variant="outlined"
-            value={comapnyData?.name}
-            onChange={(e) => handleChange("name", e.target.value)}
+          <DialogContent dividers>
+            <TextField
+              margin="dense"
+              id="name"
+              label="Company Name"
+              type="name"
+              required
+              fullWidth
+              variant="outlined"
+              value={comapnyData?.name}
+              onChange={(e) => handleChange("name", e.target.value)}
 
-          />
+            />
+            <TextField
+              margin="dense"
+              id="email"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="outlined"
+              placeholder="ex: test@test.com"
+              value={comapnyData.email}
+              onChange={(e) => handleChange("email", e.target.value)}
+            />
+
+            <TextField
+              margin="dense"
+              id="phone"
+              label="Phone no"
+              type="phone"
+              fullWidth
+              variant="outlined"
+              placeholder="0 123456789"
+              value={comapnyData.phone}
+              onChange={(e) => handleChange("phone", e.target.value)}
+            />
+
+
+            <TextField
+              id="contactPersonv"
+              margin="dense"
+              type="website"
+              fullWidth
+              required
+              label="Contact person"
+              variant="outlined"
+              value={comapnyData?.contactPerson}
+              onChange={(e) => handleChange("contactPersonv", e.target.value)}
+            />
+
+
             <TextField
               id="website"
               margin="dense"
@@ -112,22 +154,35 @@ export default function CompanyDialog({
               value={comapnyData?.website}
               onChange={(e) => handleChange("website", e.target.value)}
             >
+
+              <TextField
+                margin="dense"
+                id="noOfEmployees"
+                label="No Of Employees"
+                type="phone"
+                fullWidth
+                variant="outlined"
+                placeholder="0"
+                value={comapnyData.noOfEmployees}
+                onChange={(e) => handleChange("noOfEmployees", e.target.value)}
+              />
+
             </TextField>
-          {/* <FormControl fullWidth margin="dense"> */}
+            {/* <FormControl fullWidth margin="dense"> */}
             <InputLabel id="paymentStatus">Is Active</InputLabel>
             <Switch
               checked={comapnyData.isActive}
               onChange={(e) => handleChange("isActive", e.target.checked)}
               inputProps={{ 'aria-label': 'controlled' }}
             />
-          {/* </FormControl> */}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit" variant="contained">
-            Submit
-          </Button>
-        </DialogActions>
+            {/* </FormControl> */}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type="submit" variant="contained">
+              Submit
+            </Button>
+          </DialogActions>
         </form>
       </Dialog>
     </div>
