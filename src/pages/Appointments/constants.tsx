364661f561e2afddfbabb84496f351c6da231564
Tushar <tushar.mangla1120@gmail.com>
2025-01-14 23:53:06 +0530

    added type

--- src/pages/Appointments/constants.tsx
+++ src/pages/Appointments/constants.tsx
@@ -1,11 +1,12 @@
 import { Avatar } from "@mui/material";
 import PatientDetail from "../../components/PatientDetail";
 import DoctorDetail from "../../components/DoctorDetail";
+import LabDetail from "../../components/LabDetail";
 
 export const COLUMNS = [
     {
         header: "AVATAR",
-        accessor:"image",
+        accessor: "image",
         render: () => <Avatar
             src={"https://i.pravatar.cc/300"}
             sx={{
@@ -16,12 +17,17 @@ export const COLUMNS = [
     {
         header: "Patient NAME",
         accessor: `patient`,
-        render:(data: any)=><PatientDetail  _id={data?.patient}/>
+        render: (data: any) => <PatientDetail _id={data?.patient} />
     },
     {
-        header: "Doctor NAME",
-        accessor: "doctor",
-        render:(data: any)=><DoctorDetail  _id={data?.doctor}/>
+        header: "Type",
+        accessor: "type",
+        render: (data: any) => data.type == "1" ? "Report" : "Prescription",
+    },
+    {
+        header: "Doctor/Lab",
+        accessor: "type",
+        render: (data: any) => data.type == "1" ? (data?.lab ? <LabDetail _id={data?.lab} /> : null) : (data?.doctor ? <DoctorDetail _id={data?.doctor} /> : null),
     },
     {
         header: "Hospital",
